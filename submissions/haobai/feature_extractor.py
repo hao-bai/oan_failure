8976a62f0390b329a44590d72eb0031fff2171a0
Hao BAI <hao.bai@insa-rouen.fr>
2021-06-22 02:39:40 +0200

    HB : Add a ResNet model

--- submissions/haobai/feature_extractor.py
+++ submissions/haobai/feature_extractor.py
@@ -23,9 +23,5 @@ class FeatureExtractor:
         :return: dataset of (sample, 6720)
         :rtype: ndarray
         '''
-        # Deal with NaNs inplace
         np.nan_to_num(X, copy=False)
-        # We flatten the input, originally 3D (sample, time, dim) to
-        # 2D (sample, time * dim)
-        X = X.reshape(X.shape[0], -1)
         return X
